bdd7968a1dfc4ddeb7c60df405a5627cde7ffe48
YahyobekRahimov <rcoder570@gmail.com>
2023-09-30 17:05:23 +0500

    Project completed until the popular brands section

--- js/carousel.js
+++ js/carousel.js
@@ -1,34 +1,39 @@
-const carousel = document.querySelector('.carousel');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-
-// Define the slides variable to store all the images
-const slides = carousel.querySelectorAll('img');
-
-let currentIndex = 0;
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
+function initializeCarousel(carouselSelector) {
+    const carousel = document.querySelector(carouselSelector);
+    const prevButton = carousel.querySelector('.prev-button');
+    const nextButton = carousel.querySelector('.next-button');
+    const slides = carousel.querySelectorAll('img');
+    let currentIndex = 0;
+  
+    function showSlide(index) {
+      slides.forEach((slide, i) => {
         if (i === index) {
-            slide.style.display = 'block';
+          slide.style.display = 'block';
         } else {
-            slide.style.display = 'none';
+          slide.style.display = 'none';
         }
-    });
-}
-
-function prevSlide() {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      });
+    }
+  
+    function prevSlide() {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      showSlide(currentIndex);
+    }
+  
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % slides.length;
+      showSlide(currentIndex);
+    }
+  
+    prevButton.addEventListener('click', prevSlide);
+    nextButton.addEventListener('click', nextSlide);
+  
+    // Display the initial slide
     showSlide(currentIndex);
-}
-
-function nextSlide() {
-    currentIndex = (currentIndex + 1) % slides.length;
-    showSlide(currentIndex);
-}
-
-prevButton.addEventListener('click', prevSlide);
-nextButton.addEventListener('click', nextSlide);
-
-// Display the initial slide
-showSlide(currentIndex);
+  }
+  
+  // Initialize each carousel on your page
+  initializeCarousel('.carousel1');
+  initializeCarousel('.carousel2');
+  // Add more carousels as needed
+  
